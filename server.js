068110eb7cfa64c675fd2d4f3ba752d3b2946fb0
Deon Liang <deonyuhaoliang@gmail.com>
2019-02-17 18:10:53 -0800

    Add scrape for destinations under tomorrow

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const puppeteer = require('puppeteer');
 const url = 'http://www.yvr.ca/en/passengers/flights/departing-flights';
 const $ = require('cheerio');
 const fs = require('fs');
+const tomorrowButtonSelector = 'label[for=flights-toggle-tomorrow]';
 const YVRDestinations = {
     "Cities": []
 }
+
 const uniqueSet = new Set();
 puppeteer.launch().then(async browser => {
     const page = await browser.newPage();
@@ -14,7 +16,14 @@ puppeteer.launch().then(async browser => {
         if(uniqueSet.has($(this).text()))return true;
          uniqueSet.add($(this).text())   
      });
-    YVRDestinations.Cities =  await [...uniqueSet].sort()
+    await page.click(tomorrowButtonSelector)
+    await page.waitFor(500);
+    html = await page.content();
+    await $('td:nth-child(5)',html).each(function(i, elem) {
+        if(uniqueSet.has($(this).text()))return true;
+         uniqueSet.add($(this).text());   
+    });
+    YVRDestinations.Cities = await [...uniqueSet].sort();
             
     await fs.writeFile('YVRDestinations.json', JSON.stringify(YVRDestinations), function(err){
         if (err) console.log(err);
